dd08b75c4e5bee2bd3b62fe87f7e7e0e99508258
dldusgh318 <pololydotoly@gmail.com>
2024-09-16 02:09:45 +0900

    [FIX] dto 이름수정 (#6)

--- src/main/java/_i5i/AISecurity/web/posting/dto/PostingResponseDTO.java
+++ src/main/java/_i5i/AISecurity/web/posting/dto/PostingResponseDTO.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public class PostResponseDTO {
+public class PostingResponseDTO {
 
     @Builder
     @Getter
