eca484a38e455b2d1a9f5ad6d355857e6d40568d
dldusgh318 <pololydotoly@gmail.com>
2024-09-23 22:19:29 +0900

    [FEAT] PostingConverter 설계 (#8)

--- src/main/java/_i5i/AISecurity/web/domain/posting/converter/PostingConverter.java
+++ src/main/java/_i5i/AISecurity/web/domain/posting/converter/PostingConverter.java
@@ -1,11 +1,16 @@
 package _i5i.AISecurity.web.domain.posting.converter;
 
+import _i5i.AISecurity.web.domain.posting.dto.PostingRequestDTO;
 import _i5i.AISecurity.web.domain.posting.dto.PostingResponseDTO;
 import _i5i.AISecurity.web.domain.posting.entity.Posting;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
+@RequiredArgsConstructor
 public class PostingConverter {
 
     public static PostingResponseDTO.postingListDTO toPostingListDTO(Page<Posting> postingList){
@@ -20,6 +25,13 @@ public class PostingConverter {
                 .postingId(posting.getId())
                 .title(posting.getTitle())
                 .createdAt(posting.getCreatedAt())
+                .build();
+    }
+
+    public static Posting toEntity(PostingRequestDTO.PostingUploadRequestDTO dto, String contentHtml){
+        return Posting.builder()
+                .title(dto.getTitle())
+                .content(contentHtml)
                 .build();
     }
 }
