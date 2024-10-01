869b5a862a2087f411375707bb22d90fffefb830
dldusgh318 <pololydotoly@gmail.com>
2024-10-02 02:52:52 +0900

    [FEAT] service 통합후 설계 (#10)

--- src/main/java/_i5i/AISecurity/web/service/PostingService.java
+++ src/main/java/_i5i/AISecurity/web/service/PostingService.java
@@ -5,18 +5,28 @@ import _i5i.AISecurity.web.domain.member.entity.Member;
 import _i5i.AISecurity.web.domain.member.repository.MemberRepository;
 import _i5i.AISecurity.web.domain.posting.converter.PostingConverter;
 import _i5i.AISecurity.web.domain.posting.dto.PostingRequestDTO;
+import _i5i.AISecurity.web.domain.posting.dto.PostingResponseDTO;
 import _i5i.AISecurity.web.domain.posting.entity.Posting;
 import _i5i.AISecurity.web.domain.posting.repository.PostingRepository;
 import _i5i.AISecurity.web.handler.MemberHandler;
+import _i5i.AISecurity.web.handler.PostingHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
-public class PostingUploadService {
+public class PostingService {
+
     private final PostingRepository postingRepository;
     private final MemberRepository memberRepository;
+
+    public Page<Posting> getPostingList(Long memberId, Integer page){
+        Member member=memberRepository.findById(memberId)
+                .orElseThrow(()->new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
+        return postingRepository.findAllByMember(member, PageRequest.of(page,8));
+    }
 
     public Posting createPosting(PostingRequestDTO.PostingUploadRequestDTO dto, Long memberId){
         Member member=memberRepository.findById(memberId)
@@ -24,4 +34,11 @@ public class PostingUploadService {
         Posting posting= PostingConverter.toEntity(dto,member);
         return postingRepository.save(posting);
     }
+
+    public PostingResponseDTO.postingcontentDTO getPosting(Long postingId){
+        Posting posting=postingRepository.findById(postingId)
+                .orElseThrow(()->new PostingHandler(ErrorStatus.POSTING_NOT_FOUND));
+        return PostingConverter.toPostingcontentDTO(posting,posting.getMember().getName());
+    }
 }
+
