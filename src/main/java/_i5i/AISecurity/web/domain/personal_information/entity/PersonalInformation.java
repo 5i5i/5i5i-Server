869863bde85e9c8c63c0dc413655443e3b10d12f
dldusgh318 <pololydotoly@gmail.com>
2024-10-02 03:07:49 +0900

    [FEAT] 엔티티에 속성 추가 (#12)

--- src/main/java/_i5i/AISecurity/web/domain/personal_information/entity/PersonalInformation.java
+++ src/main/java/_i5i/AISecurity/web/domain/personal_information/entity/PersonalInformation.java
@@ -2,6 +2,7 @@ package _i5i.AISecurity.web.domain.personal_information.entity;
 
 import _i5i.AISecurity.web.domain.member.entity.Member;
 import jakarta.persistence.*;
+import jdk.jfr.Description;
 import lombok.*;
 
 @Entity
@@ -12,6 +13,17 @@ import lombok.*;
 public class PersonalInformation {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+
+    private String phoneNum;
+
+    private String address;
+
+    @Description("남자는0, 여자는1")
+    private Boolean gender;
+
+    private String birth;
 
     @ManyToOne @JoinColumn(name = "member_id")
     private Member member;
