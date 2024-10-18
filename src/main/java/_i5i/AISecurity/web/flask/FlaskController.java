de22db6956d8c0fabf17b42f923a60458f803e40
dldusgh318 <pololydotoly@gmail.com>
2024-10-18 23:47:07 +0900

    FEAT: service & service 설계 (#16)

--- src/main/java/_i5i/AISecurity/web/flask/FlaskController.java
+++ src/main/java/_i5i/AISecurity/web/flask/FlaskController.java
@@ -1,7 +1,9 @@
 package _i5i.AISecurity.web.flask;
 
+import _i5i.AISecurity.web.domain.posting.dto.PostingRequestDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +14,8 @@ public class FlaskController {
 
     private final FlaskService flaskService;
 
-    @PostMapping("/flask")
-    public String sendToFlask(@RequestBody RequestSendToFlaskDto dto) throws JsonProcessingException {
-        return flaskService.sendToFlask(dto);
+    @PostMapping("/flask/{memberId}")
+    public String sendToFlask(@PathVariable Long memberId, @RequestBody PostingRequestDTO.PostingUploadRequestDTO dto) throws JsonProcessingException {
+        return flaskService.sendToFlask(memberId,dto);
     }
 }
--- src/main/java/_i5i/AISecurity/web/flask/FlaskService.java
+++ src/main/java/_i5i/AISecurity/web/flask/FlaskService.java
@@ -1,5 +1,14 @@
 package _i5i.AISecurity.web.flask;
 
+
+import _i5i.AISecurity.apiPayload.code.status.ErrorStatus;
+import _i5i.AISecurity.web.domain.member.entity.Member;
+import _i5i.AISecurity.web.domain.member.repository.MemberRepository;
+import _i5i.AISecurity.web.domain.personal_information.entity.PersonalInformation;
+import _i5i.AISecurity.web.domain.personal_information.repository.PersonalInformationRepository;
+import _i5i.AISecurity.web.domain.posting.dto.PostingRequestDTO;
+import _i5i.AISecurity.web.handler.MemberHandler;
+import _i5i.AISecurity.web.handler.PersonalInformationHandler;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
@@ -10,15 +19,27 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 @Service
 @RequiredArgsConstructor
 public class FlaskService {
     //데이터를 JSON 객체로 변환하기 위해서 사용
     private final ObjectMapper objectMapper;
+    private final PersonalInformationRepository personalInfRepository;
+    private final MemberRepository memberRepository;
 
     @Transactional
-    public String sendToFlask(RequestSendToFlaskDto dto) throws JsonProcessingException {
+    public String sendToFlask(Long memberId, PostingRequestDTO.PostingUploadRequestDTO dto) throws JsonProcessingException {
+
+        Member member=memberRepository.findById(memberId)
+                .orElseThrow(()->new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
+        PersonalInformation personalInf=personalInfRepository.findByMember(member)
+                .orElseThrow(()->new PersonalInformationHandler(ErrorStatus.PERSONALINFORMATION_NOT_FOUND));
+
         RestTemplate restTemplate = new RestTemplate();
 
         //헤더를 JSON으로 설정함
@@ -27,7 +48,15 @@ public class FlaskService {
         //파라미터로 들어온 dto를 JSON 객체로 변환
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        String param = objectMapper.writeValueAsString(dto);
+        Map<String, Object> requestData = new HashMap<>();
+        requestData.put("content", dto.getContent());
+        requestData.put("personalInf-address", personalInf.getAddress());
+        requestData.put("personalInf-birth",personalInf.getBirth());
+        requestData.put("personalInf-gender",personalInf.getGender());
+        requestData.put("personalInf-name",personalInf.getName());
+        requestData.put("personalInf-phonenum",personalInf.getPhoneNum());
+
+        String param = objectMapper.writeValueAsString(requestData);
 
         HttpEntity<String> entity = new HttpEntity<String>(param , headers);
 
