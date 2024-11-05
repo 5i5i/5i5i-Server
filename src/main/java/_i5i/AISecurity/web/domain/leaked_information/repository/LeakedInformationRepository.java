1b2bd663e2c9f3968c2d21dd0dcfe6d6e909c454
dldusgh318 <pololydotoly@gmail.com>
2024-11-05 14:25:56 +0900

    [MODIFY] Repository findAllBy~ -> findBy로 수정 (#35)

--- src/main/java/_i5i/AISecurity/web/domain/leaked_information/repository/LeakedInformationRepository.java
+++ src/main/java/_i5i/AISecurity/web/domain/leaked_information/repository/LeakedInformationRepository.java
@@ -6,8 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LeakedInformationRepository extends JpaRepository<LeakedInformation,Long> {
-    List<LeakedInformation> findAllByPosting(Posting posting);
+    Optional<LeakedInformation> findByPosting(Posting posting);
 }
